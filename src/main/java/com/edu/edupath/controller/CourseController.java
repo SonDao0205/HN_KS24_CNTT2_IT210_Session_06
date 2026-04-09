8abe0b300da485aaed1047772bece97a2b15f29e
Dao Truong Son <daotruongson2006@gmail.com>
2026-04-09 14:31:40 +0700

    submit

--- src/main/java/com/edu/edupath/controller/CourseController.java
+++ src/main/java/com/edu/edupath/controller/CourseController.java
@@ -2,9 +2,14 @@ package com.edu.edupath.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class CourseController {
+    @GetMapping
+    public String index() {
+        return "main-layout";
+    }
 }
